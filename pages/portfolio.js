912910884899125cb018eebfd9529fca46174b6b
Wilson Cheah <wilsoncheah5@gmail.com>
2021-09-21 21:26:28 -0700

    minor update to main page

--- pages/portfolio.js
+++ pages/portfolio.js
@@ -11,7 +11,9 @@ export default function Portfolio() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <NavBar />
-      <h1>hi</h1>
+      <section>
+        <h1>hello</h1>
+      </section>
     </div>
   )
 };

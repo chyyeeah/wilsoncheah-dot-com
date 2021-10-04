9cdd4334ebfbe2ec4cbcfccc89c3d3807c8c55dc
chyyeeah <wilsoncheah5@gmail.com>
2021-10-04 14:10:44 -0700

    created a header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Head from 'next/head';
+
+const Header = () => (
+  <Head>
+    <title>Wilson Cheah</title>
+    <meta name="description" content="Wilson Cheah's personal website." />
+    <link rel="icon" href="/favicon.ico" />
+  </Head>
+);
+
+export default Header;

c4f8b25ad266926d81c0ebd71f66c27692c706b9
chyyeeah <wilsoncheah5@gmail.com>
2021-10-06 11:19:10 -0700

    - added the rest of my project photos - changed each portfolio card to be a link - updated some project data

--- src/components/Portfolio/PortfolioCard.jsx
+++ src/components/Portfolio/PortfolioCard.jsx
@@ -1,20 +1,22 @@
 import React from 'react';
 import Image from 'next/image';
 
-const PortfolioCard = ({ title, description, tech, imagePath }) => {
+const PortfolioCard = ({ title, description, tech, imagePath, github }) => {
   return (
-    <div className='p-4 text-center border border-black rounded-lg border-opacity-25 shadow-md bg-white'>
-      <div className='pt-2 pb-4'>
-        <span className='text-3xl font-extralight'>{title}</span>
+    <a href={github} target='_blank' rel='noreferrer'>
+      <div className='p-4 text-center border border-black rounded-lg border-opacity-25 shadow-md bg-white'>
+        <div className='pt-2 pb-4'>
+          <span className='text-3xl font-extralight'>{title}</span>
+        </div>
+        <div>
+          <Image src={imagePath} alt={title} width={270} height={200} />
+        </div>
+        <p className='text-sm text-gray-500'>
+          {tech.join(', ')}
+        </p>
+        <p className='pt-2 tracking-wide leading-6 text-md'>{description}</p>
       </div>
-      <div>
-        <Image src={imagePath} alt={title} width={270} height={200} />
-      </div>
-      <p className='text-sm text-gray-500'>
-        {tech.join(', ')}
-      </p>
-      <p className='pt-2 tracking-wide leading-6 text-md'>{description}</p>
-    </div>
+    </a>
   );
 };
 
--- src/data/projects.js
+++ src/data/projects.js
@@ -1,37 +1,50 @@
 module.exports = [
   {
-    id: 0,
-    title: 'Questions & Answers',
-    description: 'Built with the inquiring consumer in mind. There are times when the product description is not enough. This gives the consumer the ability to reach out to fellow consumers and even the seller themselves.',
-    tech: ['React', 'React Hooks', 'Higher Order Components'],
-    imagePath: '/img/Q&A.png',
-  },
-  {
-    id: 1,
-    title: 'Kanye Says',
-    description: `A simple interface to access the wonderful world of Kanye's twitter feed.`,
-    tech: ['React', 'JWT', 'React Router'],
-    imagePath: '/img/kanyesays.png'
-  },
-  {
     id: 2,
     title: 'Correspond',
     description: `A real-time chat application that connects you with pals around the world. You can choose to talk to someone in a specific country or we can pick for you. `,
-    tech: ['React', 'Session-based Authentication', 'WebSockets', 'MongoDB'],
-    imagePath: '/img/correspond.png'
+    tech: ['React', 'Session-based Authentication', 'WebSockets', 'MongoDB', 'Node'],
+    imagePath: '/img/correspond.png',
+    github: 'https://github.com/Correspond-Web-App/main'
   },
   {
     id: 3,
     title: 'Flik',
     description: `Wish you were better at your favorite first person shooter? With Flik, you can practice your reaction time and accuracy with targets that will appear at random on your screen!`,
-    tech: ['React', 'Nivo (extension of D3)'],
-    imagePath: '/img/flik.png'
+    tech: ['React', 'Nivo (extension of D3)', 'Node'],
+    imagePath: '/img/flik.png',
+    github: 'https://github.com/chyyeeah/flik'
+  },
+  {
+    id: 1,
+    title: 'Kanye Says',
+    description: `A simple interface to access the wonderful world of Kanye's twitter feed.`,
+    tech: ['React', 'JWT', 'React Router', 'Node'],
+    imagePath: '/img/kanyesays.png',
+    github: 'https://github.com/chyyeeah/kanye-says'
+  },
+  {
+    id: 5,
+    title: 'Color Guessing Game',
+    description: `Test your knowledge of RGB values!`,
+    tech: ['React', 'React Router', 'Node'],
+    imagePath: '/img/colorgame.png',
+    github: 'https://github.com/chyyeeah/color-guessing-game'
+  },
+  {
+    id: 0,
+    title: 'Fangorn',
+    description: 'Built with the inquiring consumer in mind. There are times when the product description is not enough. This gives the consumer the ability to reach out to fellow consumers and even the seller themselves.',
+    tech: ['React', 'React Hooks', 'Higher Order Components'],
+    imagePath: '/img/Q&A.png',
+    github: 'https://github.com/fellowship-of-the-code-sj/Fangorn'
   },
   {
     id: 4,
     title: 'Facade',
     description: `Want to talk to the masses? With Facade, you can begin broadcasting and all you need to do is share your room name with your loyal fans!`,
     tech: ['WebRTC', 'WebSockets'],
-    imagePath: 'https://cdn.pixabay.com/photo/2021/03/05/21/43/lizard-6072391_960_720.jpg'
+    imagePath: '/img/facade.png',
+    github: 'https://github.com/chyyeeah/facade'
   }
 ];

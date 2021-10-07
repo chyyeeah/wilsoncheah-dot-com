b089094afc831722f2b1161ae2d6393351a9c90a
chyyeeah <wilsoncheah5@gmail.com>
2021-10-06 20:12:52 -0700

    added some info the the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,9 @@ import Head from 'next/head';
 const Header = () => (
   <Head>
     <title>Wilson Cheah</title>
-    <meta name="description" content="Wilson Cheah's personal website." />
+    <meta name="description" content="Wilson Cheah's personal website that showcases some of the projects he has worked on." />
+    <meta name="author" content="Wilson Cheah"></meta>
+    <meta name="image" property="og:image" content="/christopher-gower-m_HRfLhgABo-unsplash.jpg" />
     <link rel="icon" href="/favicon.ico" />
   </Head>
 );
